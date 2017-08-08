ffe4817074ac9746a9b65b56b4d96233a89ecfa2
carloscacho <carlosemilio.ac@gmail.com>
2017-08-08 18:19:01 -0400

    create component
    create first component, it is generate content header

---
+++ projectWeb/angular1/app/common/components/contentHeader.js
@@ -0,0 +1,11 @@
+angular.module('appPrincipal').component('contentHeader', {
+  bindings:{
+    name: '@',
+    small: '@'
+  },
+  template: `
+  <section class="content-header">
+    <h1>{{$ctrl.name}} <small> {{$ctrl.small}} </small></h1>
+  </section>
+  `
+})
