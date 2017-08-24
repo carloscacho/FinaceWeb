e414f4f9f0c2478fb82103d9ce4eff20c771209e
carloscacho <carlosemilio.ac@gmail.com>
2017-08-24 16:37:55 -0400

    Revert "paginator"
    This reverts commit f856c7791d91e84ce906c4deba483dde416d7d56.

--- projectWeb/backend/config/routes.js
+++ projectWeb/backend/config/routes.js
@@ -9,30 +9,13 @@ module.exports = function(server) {
     const openApi = express.Router()
     server.use('/oapi', openApi)
 
-<<<<<<< HEAD
-    const AuthService = require('../api/user/authService')
-    openApi.post('/login', AuthService.login)
-    openApi.post('/signup', AuthService.signup)
-    openApi.post('/validateToken', AuthService.validateToken)
-    /*
-    * Rotas protegidas por Token JWT
-    */
-    const protectedApi = express.Router()
-    server.use('/api', protectedApi)
-    protectedApi.use(auth)
-
-    const billingCycleService = require('../api/finance/financeService')
-    billingCycleService.register(protectedApi, '/finance')
-
-    const billingSummaryService = require('../api/financeSummary/financeSummaryService')
-    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
-
-=======
     //rotas da api
     const finaceService = require('../api/finace/financeService');
     finaceService.register(router, '/finance')
 
     const financeSummaryService = require('../api/finaceSummary/financeSummaryService')
+<<<<<<< HEAD
+=======
 >>>>>>> parent of f856c77... paginator
 
 }
