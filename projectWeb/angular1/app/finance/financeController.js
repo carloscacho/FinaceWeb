76ad305145d0b186d5ce4cde418a750ced2998e7
carloscacho <carlosemilio.ac@gmail.com>
2018-03-13 16:20:18 -0400

    Funcionalidades dos botões
    Recuperação das funcionalidade dos botões das lista de credito e debito

--- projectWeb/angular1/app/finance/financeController.js
+++ projectWeb/angular1/app/finance/financeController.js
@@ -60,6 +60,43 @@
           })
         }
 
+        //implementação dos botoes de ação
+        //botão de adicionar credito
+        vm.btnAddCredit = function(index){
+          vm.finance.credits.splice(index+1, 0,{});
+        }
+
+        //botão de clonar credito
+        vm.btnCloneCredit = function(index, {name, value}){
+          vm.finance.credits.splice(index+1, 0, {name, value});  
+        }
+
+        //botão de deletar Credito
+        vm.btnDeleteCredit = function(index){
+          if(vm.finance.credits.length > 1){
+            vm.finance.credits.splice(index, 1);
+          } 
+        }
+
+       //implementação dos botoes de ação  de delete
+        //botão de adicionar credito
+        vm.btnAddDebit = function(index){
+          vm.finance.debts.splice(index + 1, 0,{});
+        }
+
+        //botão de clonar credito
+        vm.btnCloneDebit = function(index, {name, value, status}){
+          vm.finance.debts.splice(index + 1, 0, {name, value, status});  
+        }
+
+        //botão de deletar Credito
+        vm.btnDeleteDebit = function(index){
+          if(vm.finance.debts.length > 1){
+            vm.finance.debts.splice(index, 1);
+          } 
+        }
+
+
         vm.refresh()
     }
 })()
