2d0a3b66bfa0a0dc036771bfe3c96cfea813d44d
carloscacho <carlosemilio.ac@gmail.com>
2018-03-19 15:49:51 -0400

    update for deploy
    tentetiva de deploy no heroku

--- projectWeb/backend/config/database.js
+++ projectWeb/backend/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/db_finace')
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/db_finace'
+module.exports = mongoose.connect(url, {useMongoClient: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio!"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
