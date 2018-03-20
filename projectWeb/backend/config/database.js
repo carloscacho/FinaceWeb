3c773fd31ad823f40cc69211ec18da8616d8237d
carloscacho <carlosemilio.ac@gmail.com>
2018-03-20 11:38:08 -0400

    Revert "update for deploy"
    This reverts commit 2d0a3b66bfa0a0dc036771bfe3c96cfea813d44d.

--- projectWeb/backend/config/database.js
+++ projectWeb/backend/config/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/db_finace'
-module.exports = mongoose.connect(url, {useMongoClient: true})
+module.exports = mongoose.connect('mongodb://localhost/db_finace')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio!"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
